51e497b5132fd2458ef28ba5140242c1fc971756
Matteo Santucci <matteosantucci@EmmesAirbook3.local>
2022-07-19 12:50:41 +0200

    import/export method changed

---
+++ client/src/local-data/menu.js
@@ -0,0 +1,14 @@
+const data = {
+  menu: [
+    { type: 'contacts', data: '' },
+    { type: 'about us', data: '' },
+    { type: 'privacy', data: '' },
+  ],
+  links: [
+    { type: 'facebook', link: '', logo: '' },
+    { type: 'youtube', link: '', logo: '' },
+    { type: 'twitter', link: '', logo: '' },
+  ],
+}
+
+export default data
