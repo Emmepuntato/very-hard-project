be8f24004b35cffd2eb2558583d643b977fa2330
Matteo Santucci <matteosantucci@EmmesAirbook3.local>
2022-07-19 14:06:31 +0200

    add some animation

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { BiMenu } from 'react-icons/bi'
+
 import Menu from './Menu'
 function Navbar() {
-  const [size, setSize] = useState('')
+  const [isSidebar, setIsSidebar] = useState(false)
   const navbar = useRef()
 
   useEffect(() => {}, [])
   return (
     <nav>
       <div className='header' ref={navbar}>
-        {size}
         <div className='nav-logo'>logo</div>
         <Menu />
         <div className='nav-links'>links</div>
+        <BiMenu
+          className={`${isSidebar ? 'sidebar-icon-show' : 'sidebar-icon'}`}
+          onClick={() => {
+            setIsSidebar(!isSidebar)
+          }}
+        />
       </div>
     </nav>
   )
