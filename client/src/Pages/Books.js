015064961314a3015a5eaac4d494fae264e0a0ca
Matteo Santucci <matteosantucci@EmmesAirbook3.local>
2022-07-22 19:14:39 +0200

    books'page rendering in process

---
+++ client/src/Pages/Books.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import Search from '../Components/Search'
+import Filter from '../Components/Filter'
+import Shelf from '../Components/Shelf'
+
+function BookPage() {
+  return (
+    <section className='bookpage'>
+      <div className='search'>
+        <div className='center'>
+          <Search />
+        </div>
+      </div>
+      <div className='filter'>
+        <div className='center'>
+          <Filter />
+        </div>
+      </div>
+      <div className='shelf'>
+        <Shelf />
+      </div>
+    </section>
+  )
+}
+
+export default BookPage
