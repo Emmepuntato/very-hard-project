14eb184b9988e7982b3ceeb01431bf58c4d48b12
Emmepuntato <matsantucci36@gmail.com>
2022-07-22 21:41:17 +0200

    create reducer

---
+++ client/src/local-data/reducer.js
@@ -0,0 +1,8 @@
+export const reducer = (state, action)=>{
+    switch(action.type){
+        case 'SEARCH':
+            return
+        default:
+            throw new Error('No Dispatch Found')
+    }
+}
