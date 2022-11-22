eb30c9a1b77d41134fadf72007b1f7f65b20f5ac
Emmepuntato_ <matsantucci36@gmail.com>
2022-11-22 09:55:13 +0100

    func update and page system added

--- client/src/Components/Shelf.js
+++ client/src/Components/Shelf.js
@@ -9,6 +9,9 @@ function Shelf() {
   const API_KEY = 'AIzaSyCstauv1GWKGRuQ5XyUWfSsy9_SUXbFy7I'
   const [books, setBooks] = useState([])
   const [itemCount, setItemCount] = useState([])
+  const [pageNum, setPageNum] = useState(0)
+  const itemDisplayed = 18
+  const [starting, setStarting] = useState(0)
 
   // const customURL = `https://www.googleapis.com/books/v1/volumes?q=${
   //   bookSearchInput.term || 'welcome'
@@ -27,23 +30,41 @@ function Shelf() {
       setItemCount(data.totalItems)
       setIsLoading(false)
       console.log('API successful')
-      console.log('books', books)
+      console.log('books', books, 'type of books', typeof books)
     } catch (error) {
-      console.log('error ', error)
+      console.log('error -> ', error)
     }
   }
 
   useEffect(() => {
     console.log('searcURL re-rendered')
-    fetchBooks(searchURL)
-  }, [searchURL])
+    fetchBooks(searchURL.concat(`&startIndex=${starting}`))
+    window.scrollTo(0, 0)
+  }, [searchURL, starting])
 
   useEffect(() => {
     console.log('filteredURL re-rendered')
-    fetchBooks(filteredURL)
-  }, [filteredURL])
+    fetchBooks(filteredURL.concat(`&startIndex=${starting}`))
+    window.scrollTo(0, 0)
+  }, [filteredURL, starting])
 
   console.log('elements found ', itemCount)
+
+  const pageCounter = (operator) => {
+    // operator === '-'
+    //   ? pageNum >= 1 && setPageNum(pageNum - 1)
+    //   : setPageNum(pageNum + 1)
+    switch (operator) {
+      case '-':
+        pageNum >= 1 && setPageNum(pageNum - 1)
+        setStarting(starting - itemDisplayed)
+        break
+      case '+':
+        if (starting > itemCount) break
+        setStarting(starting + itemDisplayed)
+        setPageNum(pageNum + 1)
+    }
+  }
 
   if (isLoading) {
     return <Loading />
@@ -52,57 +73,79 @@ function Shelf() {
   if (itemCount < 1 || itemCount === undefined) {
     return <div>'no results :('</div>
   }
-
   return (
-    <div className='shelf-grid'>
-      {books.map((item, index) => {
-        const {
-          authors,
-          categories = '---//---',
-          imageLinks = altThumbnail,
-          publisher = '---//---',
-          title = '---//---',
-        } = item.volumeInfo
-        const id = item.id
-        let authorsList
+    <>
+      <div className='shelf-grid'>
+        {books.map((item, index) => {
+          const {
+            authors,
+            categories = '---//---',
+            imageLinks = altThumbnail,
+            publisher = '---//---',
+            title = '---//---',
+          } = item.volumeInfo
+          const id = item.id
+          let authorsList
 
-        if (authors !== undefined) {
-          authorsList = authors.join(', ')
-        }
-        return (
-          <article className='shelf-book' key={index}>
-            <Link to={`/books/${id}`}>
-              <img
-                src={imageLinks.thumbnail || ''}
-                alt='thumbnail'
-                style={{ width: '12rem', height: '16rem' }}
-              />
-            </Link>
-            <p
-              style={{
-                fontSize: '1.5rem',
-                maxHeight: '4rem',
-                textAlign: 'center',
-                overflow: 'hidden',
-                fontWeight: '500',
-              }}
-            >
-              {title.substring(0, 35)}
-            </p>
-            <p
-              style={{
-                fontStyle: 'italic',
-                margin: '0.2rem',
-                fontSize: '1.1rem',
-              }}
-            >
-              {authorsList || authors}
-            </p>
-            <p style={{ fontSize: '0.9rem', margin: '0' }}>- {categories} -</p>
-          </article>
-        )
-      })}
-    </div>
+          if (authors !== undefined) {
+            authorsList = authors.join(', ')
+          }
+          return (
+            <article className='shelf-book' key={index}>
+              <Link to={`/books/${id}`}>
+                <img
+                  src={imageLinks.thumbnail || ''}
+                  alt='thumbnail'
+                  style={{ width: '12rem', height: '16rem' }}
+                />
+              </Link>
+              <p
+                style={{
+                  fontSize: '1.5rem',
+                  maxHeight: '4rem',
+                  textAlign: 'center',
+                  overflow: 'hidden',
+                  fontWeight: '500',
+                }}
+              >
+                {title.substring(0, 35)}
+              </p>
+              <p
+                style={{
+                  fontStyle: 'italic',
+                  margin: '0.2rem',
+                  fontSize: '1.1rem',
+                }}
+              >
+                {authorsList || authors}
+              </p>
+              <p style={{ fontSize: '0.9rem', margin: '0' }}>
+                - {categories} -
+              </p>
+            </article>
+          )
+        })}
+      </div>
+      <footer className='shelf-footer'>
+        <div>
+          <button
+            onClick={() => {
+              pageCounter('-')
+            }}
+          >
+            previous page
+          </button>
+          {pageNum + 1}
+          <button
+            onClick={() => {
+              pageCounter('+')
+            }}
+          >
+            next page
+          </button>
+        </div>
+      </footer>
+    </>
   )
 }
 
